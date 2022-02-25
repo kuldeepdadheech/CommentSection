8f4783b1e93f4774fde3b3194335546a3623179f
AkshitaDadheech <dadheechakshita@gmail.com>
2022-02-25 23:08:45 +0530

    CS

--- index.js
+++ index.js
@@ -1,75 +1,86 @@
-let likeCounter=0;
-const commentDisplay = document.getElementById("displayComments");
+//For storing the comments
+window.onload = function setTemplate() {
+    document.getElementById('allComments').innerHTML = localStorage.getItem('template');
+};
 
+const commentContainer = document.getElementById('allComments');
 
-//Adding event listener to add button for calling addComment function:
-document.getElementById("Add").addEventListener("click",(e)=>{
-    addComment(e);
-})
+//Adding event listener to addcomment button
+document.getElementById('addComments').addEventListener('click', function (ev) {
+   addComment(ev);
+});
 
-
-//Add Comment function for adding comments:
-function addComment(){
-    let comment = document.getElementById("comment");
-    let newcontainer=document.createElement("div");
-    newcontainer.id = "new-container";
-    let newcomment = comment;
-    newcomment.id = "new-comment";
-    let container = document.getElementById("container");
-    let replyButton = document.createElement("button");
-    replyButton.innerHTML="Reply";
-    replyButton.addEventListener("click",()=>{
-        addReply(newcontainer);
-    });
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML="Delete";
-    deleteButton.addEventListener("click",()=>{
-        newcontainer.remove();
-    });
-    let likeButton = document.createElement("button");
-    likeButton.innerHTML="Like";
-    likeButton.addEventListener("click",()=>{
-        likeCounter=addLike();
-        likeButton.innerHTML="Likes. "+likeCounter ;
-    });
-    let buttonContainer = document.createElement("div");
-    buttonContainer.id = "button-container"
-    buttonContainer.appendChild(replyButton);
-    buttonContainer.appendChild(deleteButton);
-    buttonContainer.appendChild(likeButton);
-    newcontainer.appendChild(newcomment);
-    newcontainer.appendChild(buttonContainer);
-    container.appendChild(newcontainer);
+//Add comment function to add comments
+function addComment(ev) {
+    let commentText, wrapDiv;
+    const textBox = document.createElement('div');
+    const replyButton = document.createElement('button');
+    replyButton.className = 'reply';
+    replyButton.innerHTML = 'Reply';
+    const likeButton = document.createElement('button');
+    likeButton.innerHTML = 'Like';
+    likeButton.className = 'likeComment';
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+    deleteButton.className = 'deleteComment';
+    if(hasClass(ev.target.parentElement, 'container')) {
+        const wrapDiv = document.createElement('div');
+        wrapDiv.className = 'wrapper';
+        wrapDiv.style.marginLeft = 0;
+        commentText = document.getElementById('comment').value;
+        document.getElementById('comment').value = '';
+        textBox.innerHTML = commentText;
+        textBox.style.backgroundColor = "lightblue";
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+        commentContainer.appendChild(wrapDiv);
+    } else {
+        wrapDiv = ev.target.parentElement;
+        commentText = ev.target.parentElement.firstElementChild.value;
+        textBox.innerHTML = commentText;
+        textBox.style.backgroundColor = "paleturquoise";
+        wrapDiv.innerHTML = '';
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+    }
+    setOnLocalStorage();
 }
-function addLike(){
-        likeCounter++;
-        return likeCounter;
+
+function setOnLocalStorage () {
+    localStorage.setItem('template', document.getElementById('allComments').innerHTML);
 }
-function addReply(newcontainer){
-    let newreplycontainer = document.createElement("div");
-    let newreply = document.createElement("input");
-    newreply.type="text";
-    newreply.placeholder="enter your reply";
-    let addReplyButton=document.createElement("button");
-    addReplyButton.innerHTML="Add Reply"
-    addReplyButton.addEventListener("click",()=>{
-        addComment();
-    });
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML="Delete";
-    deleteButton.addEventListener("click",()=>{
-        newreplycontainer.remove();
-    });
-    let likeButton = document.createElement("button");
-    likeButton.innerHTML="Like";
-    likeButton.addEventListener("click",()=>{
-        likeCounter=addLike();
-        likeButton.innerHTML="Likes. "+likeCounter ;
-    });
-    newreplycontainer.appendChild(newreply);
-    newreplycontainer.appendChild(addReplyButton);
-    newreplycontainer.appendChild(likeButton);
-    newreplycontainer.appendChild(deleteButton);
-    newcontainer.appendChild(newreplycontainer);
-    
+
+//To compare class name of the button clicked
+function hasClass(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
 }
+
+
+document.getElementById('allComments').addEventListener('click', function (e) {
+    if (hasClass(e.target, 'reply')) {
+        const parentDiv = e.target.parentElement;
+        const wrapDiv = document.createElement('div');
+        wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
+        wrapDiv.className = 'wrapper';
+        const textArea = document.createElement('textarea');
+        textArea.style.marginRight = '20px';
+        const addButton = document.createElement('button');
+        addButton.className = 'addReply';
+        addButton.innerHTML = 'Add';
+        const cancelButton = document.createElement('button');
+        cancelButton.innerHTML = 'Cancel';
+        cancelButton.className='cancelReply';
+        wrapDiv.append(textArea, addButton, cancelButton);
+        parentDiv.appendChild(wrapDiv);
+
+    } else if(hasClass(e.target, 'addReply')) {
+        addComment(e);
+    } else if(hasClass(e.target, 'likeComment')) {
+         const likeBtnValue = e.target.innerHTML;
+         e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 : 1;
+        setOnLocalStorage();
+    } else if(hasClass(e.target, 'cancelReply')) {
+        e.target.parentElement.innerHTML = '';
+        setOnLocalStorage();
+    } else if(hasClass(e.target, 'deleteComment')) {
+        e.target.parentElement.remove();
+    }
+});
